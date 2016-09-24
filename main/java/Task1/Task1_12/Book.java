a5cdd19aaa83e5da0c6667fb6893b3109d5abe2d
Aliaksandr Khotsyn <Aliaksandr_Khotsyn@epam.com>
2016-09-24 20:11:32 +0300

    Task2

--- main/java/Task1/Task1_12/Book.java
+++ main/java/Task1/Task1_12/Book.java
@@ -1,10 +1,20 @@
 package Task1.Task1_12;
 
-public class Book implements Cloneable {
+public class Book implements Cloneable, Comparable<Book> {
     private String title;
     private String author;
     private int price;
+    private int isbn;
     private static int editor;
+
+    public Book(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public int compareTo(Book book) {
+        int bookIsbn = book.isbn;
+        return this.isbn - bookIsbn;
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -19,6 +29,9 @@ public class Book implements Cloneable {
         }
         Book other = (Book) obj;
         if (price != other.price) {
+            return false;
+        }
+        if (isbn != other.isbn) {
             return false;
         }
         if (null == title) {
@@ -43,6 +56,7 @@ public class Book implements Cloneable {
     public int hashCode() {
         int hash = 1;
         hash = hash * 31 + price;
+        hash = hash * 31 + isbn;
         hash = hash * 31 + (title == null ? 0 : title.hashCode());
         hash = hash * 31 + (author == null ? 0 : author.hashCode());
         return hash;
